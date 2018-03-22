4fa7a75d5cf356b04e8a44e4b59e0abe5f239f63
Daniel Ogonowski <daniel.ogonowski@gmail.com>
2018-03-22 08:12:23 +0100

    Added some methods to greetr object's prototype

--- js/greetr/greetr.js
+++ js/greetr/greetr.js
@@ -1,19 +1,79 @@
-//My own labrary project. Library creates usable greeting
+//My own library project. Library creates usable greeting.
 //Wrapping whole function in IIFE to not interfere with another scripts
 (function (global, $) {//setting parameters for this IIFE
-  var Greetr = function(firstname, lastname, language) { // Function returning function constructor to prevent from using new keyword all the time
+  var Greetr = function(firstname, lastname, language) { // Function returning function constructor to prevent from using 'new' keyword all the time
     return new Greetr.init(firstname, lastname, language);//returning new object created via function constructor
   };
 
-  Greetr.prototype = {}; //empty object to hold all properties and methods
+  //Data available to greetr object methods via closures, not accessible from the global object
+  var supportedLanguages = ['en', 'de', 'pl'];
 
-  Greetr.init = function(firstname, lastname, language) {
-    var self = this;
-    self.firstname = firstname || 'Daniel';
-    self.lastname = lastname || 'Ogonowski';
-    self.langauge = language || 'english';
+  var greetings = {
+    en: 'Hello',
+    de: 'Hallo',
+    pl: 'Cześć'
   };
 
-  Greetr.init.prototype = Greetr.prototype;
-  global.Greetr = global.G$ = Greetr;
+  var formalGreetings = {
+    en: 'Good morning',
+    de: 'Guten Tag',
+    pl: 'Dzień dobry'
+  };
+
+  var logMessages = {
+    en: 'Logged in',
+    de: 'Eingeloggt',
+    pl: 'Zalogowany'
+  };
+
+  // object which includes all properties and methods
+  Greetr.prototype = {
+
+    fullName: function() {
+      return this.firstname + ' ' + this.lastname; //'this' keyword refers to the calling object
+    },
+    greeting: function() {
+      return greetings[this.language] + ' ' + this.firstname + '!';
+    },
+    formalGreeting: function() {
+      return formalGreetings[this.language] + ', ' + this.fullName();
+    },
+    greet: function(formal) {
+      if (formal) { //null or undefined coerces to false
+        return this.formalGreeting();
+      } else {
+        return this.greeting();
+      }
+    },
+    validate: function() {
+      if(supportedLanguages.indexOf(this.language) === -1) {
+        throw new Error('Provided language is not supported');
+      } else {
+        console.log('Provided language is valid');
+      }
+    },
+    setLang: function(lang) {
+      this.language = lang;
+      this.validate();
+      return this//returning 'this' keyword gives back calling object and it makes method chainable
+    },
+    log: function() {
+      if (console) {
+        console.log(logMessages[this.language] + ': ' + this.fullName());
+      }
+    }
+  };
+
+
+
+  Greetr.init = function(firstname, lastname, language) {//funtion constructor for new created objects
+    var self = this;//prevents 'this' keyword from pointing at global object during execution context
+    self.firstname = firstname || '';//'self' refers to the calling object during execution context
+    self.lastname = lastname || '';
+    self.language = language || 'en';
+  };
+
+//Every new created object's prototype is prototype method(not __proto__ of this function!) of function which created him
+  Greetr.init.prototype = Greetr.prototype; //overwriting prototype method of function constructor with our object with methods and properites we want new created object to have acces to
+  global.Greetr = global.G$ = Greetr; //exposing our function to global object, window in this case
 }( window, jQuery));
